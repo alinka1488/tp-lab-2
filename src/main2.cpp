cedb3109fec7896517ad783a26182a896a29daf2
Andrey Talanin <talanin.aa.nnov@gmail.com>
2019-11-05 16:17:05 +0300

    Implement the functions defined in the Task 2

---
+++ src/main2.cpp
@@ -0,0 +1,21 @@
+#include "stdlib.h"
+#include <iostream>
+
+#include "task2.h"
+
+#if false
+int32_t gen()
+{
+	return rand() % 10;
+};
+
+int32_t main()
+{
+	int32_t *array = createArr<int32_t, 10>(gen);
+	for (int32_t i = 0; i < 10; i++)
+		std::cout << array[i] << std::endl;
+
+	delete[] array;
+	return 0;
+};
+#endif
