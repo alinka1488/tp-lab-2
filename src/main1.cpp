2e4f56795e0665f9e8afbbb1b19cfc5e62a1b771
aailyin <artyomgoodguy123@gmail.com>
2019-12-02 18:03:37 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,34 +1,19 @@
-#include<string.h>
-#include<iostream>
-#include"task1.h"
-#define dim 10
+#include "task1.h"
+#include <iostream>
 
-template<typename T> void msort(T* mas, int n);
-template<> void msort(char** mas, int n);
+using namespace std;
 
 int main() {
-	int a[dim] = { 0 };
-	for (int i = 0; i < dim; i++)
-	{
-		a[i] = (rand() % 100);
-		std::cout << a[i] << " ";
+	int mas[]{ 5, -9, -3, 2, 8, 4, -1, 6, 7 };
+	int n = 9;
+	msort<int>(mas, n);
+	for (int i = 0; i < n; i++) {
+		std::cout << mas[i] << " ";
 	}
-	msort<int>(a, dim);
-	std::cout << "\n";
-	for (int i = 0; i < dim; i++) {
-		std::cout << a[i] << " ";
-	}
-	std::cout << "\n";
-	char str[] = "qwerty";
-	msort<char>(str, strlen(str));
-	for (int i = 0; i < strlen(str); i++) {
-		std::cout << str[i];
-	}
-	std::cout << "\n";
-	char* s[6]{ (char*)"aaaaa", (char*)"aaaa", (char*)"aa", (char*)"a", (char*)"a", (char*)"aaa" };
-	msort<char*>(s, 6);
+        std::cout << endl;
+	char* val[6]{ (char*)"ffff", (char*)"fff2", (char*)"f", (char*)"ff", (char*)"vvv", (char*)"fffrr" };
+	msort(val, 6);
 	for (int i = 0; i < 6; i++) {
-		std::cout << s[i] << " ";
+		std::cout << val[i] << endl;
 	}
-	return 0;
-}
+} 
