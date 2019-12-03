9dd67faa7f0f105d286cf43b0617eb1fd612280a
Andrew who don't know programming <agbazarkin@edu.hse.ru>
2019-12-03 16:20:38 +0300

    version 8

--- include/task1.h
+++ include/task1.h
@@ -1,19 +1,19 @@
 #pragma once
 #include <iostream>
-#include <string.h>
+#include <cstring>
+#include <cstddef>
 
 
 template<typename T>
 bool cmp(T left, T right){
-	if (left <= right)
+	if (left < right)
 		return 1;
 	else
 		return 0;
 }
 
-template<typename T>
 bool cmp(char* left, char* right){
-	if (strlen(left) <= strlen(right))
+	if (strlen(left) < strlen(right))
 		return 1;
 	else
 		return 0;
@@ -30,7 +30,7 @@ void msort(T mas[], int size){
 	int right = size / 2;
 	int count = 0;
 	while(left < size / 2 && right < size){
-		if(cmp(mas[left], mas[right]) == 1)
+		if(cmp(mas[left], mas[right]))
 			buf[count++] = mas[left++];
 		else
 			buf[count++] = mas[right++];
--- src/main1.cpp
+++ src/main1.cpp
@@ -2,14 +2,11 @@
 
 
 int main(){
-	int mas[]= {1, 5, 8, 2, 4, 9, 6};
-	float massiv[] = {1.2, 6.7, 2.3, 9.0, 4.5};
-	char* stroka[6]{ (char*)"12345", (char*)"1234", (char*)"123", (char*)"1", (char*)"12", (char*)"123456" };
+	int mas[]= {1, 5, 8, 2, 4, 6};
+	float massiv[] = {1.2, 6.7, 2.3, 9.0, 4.5, 6.8};
 	int size1 = sizeof(mas) / sizeof(int);
 	int size2 = sizeof(massiv) / sizeof(float);
-	int size3 = sizeof(stroka) / sizeof(char*);
 	msort<int>(mas, size1);
 	msort<float>(massiv, size2);
-	msort<char>(stroka, size3);
 	return 0;
 }
