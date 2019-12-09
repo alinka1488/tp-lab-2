8162a9c10b347db6a67b18f47c3fd7f8760968f0
aailyin <artyomgoodguy123@gmail.com>
2019-12-09 16:16:56 +0300

    10

--- include/task1.h
+++ include/task1.h
@@ -3,75 +3,76 @@
 
 template<typename T> void msort(T* mas, int n) {
 	if (n > 1) {
-		int mid = n / 2, i = 0, j = 0, k = 0;
-		T* left = new T[mid];
-		T* right = new T[n - mid];
+		int mid = n / 2, i, j, k;
+		T* leftHalf = new T[mid];
+		T* rightHalf = new T[n - mid];
 		for (i = 0; i < mid; i++) {
-			left[i] = mas[i];
-			right[i] = mas[mid + i];
+			leftHalf[i] = mas[i];
+			rightHalf[i] = mas[mid + i];
 		}
-		if (n % 2 != 0) {
-			right[mid] = mas[n - 1];
-		}
-		msort(left, mid);
-		msort(right, n - mid);
+		if (n % 2 != 0) rightHalf[mid] = mas[n - 1];
+		msort(leftHalf, mid);
+		msort(rightHalf, n - mid);
 		i = 0;
+		j = 0;
+		k = 0;
 		for (; k < n; k++) {
 			if (i > mid - 1) {
-				mas[k] = right[j];
+				mas[k] = rightHalf[j];
 				j++;
 			}
 			else if (j > n - mid - 1) {
-				mas[k] = left[i];
+				mas[k] = leftHalf[i];
 				i++;
 			}
-			else if (left[i] < right[j]) {
-				mas[k] = left[i];
+			else if (leftHalf[i] < rightHalf[j]) {
+				mas[k] = leftHalf[i];
 				i++;
 			}
 			else {
-				mas[k] = right[j];
+				mas[k] = rightHalf[j];
 				j++;
 			}
 		}
-		delete[] left;
-		delete[] right;
+		delete[] leftHalf;
+		delete[] rightHalf;
 	}
 }
-template<> void msort(char** mas, int n) {
+template<> void msort(char **mas, int n) {
 	if (n > 1) {
-		int mid = n / 2, i = 0, j = 0, k = 0;
-		char** left = new char* [mid];
-		char** right = new char* [n - mid];
+		int mid = n / 2, i, j, k;
+		char** leftHalf = new char*[mid];
+		char** rightHalf = new char *[n - mid];
 		for (i = 0; i < mid; i++) {
-			left[i] = mas[i];
-			right[i] = mas[mid + i];
+			leftHalf[i] = mas[i];
+			rightHalf[i] = mas[mid + i];
 		}
-		if (n % 2 != 0) {
-			right[mid] = mas[n - 1];
-		}
-		msort(left, mid);
-		msort(right, n - mid);
+		if (n % 2 != 0) rightHalf[mid] = mas[n - 1];
+		msort(leftHalf, mid);
+		msort(rightHalf, n - mid);
 		i = 0;
+		j = 0;
+		k = 0
 		for (; k < n; k++) {
 			if (i > mid - 1) {
-				mas[k] = right[j];
+				mas[k] = rightHalf[j];
 				j++;
 			}
 			else if (j > n - mid - 1) {
-				mas[k] = left[i];
+				mas[k] = leftHalf[i];
 				i++;
 			}
-			else if (strlen(left[i]) < strlen(right[j])) {
-				mas[k] = left[i];
+			else if (strlen(leftHalf[i]) < strlen(rightHalf[j])) {
+				mas[k] = leftHalf[i];
 				i++;
 			}
 			else {
-				mas[k] = right[j];
+				mas[k] = rightHalf[j];
 				j++;
 			}
 		}
-		delete[] left;
-		delete[] right;
+		delete[] leftHalf;
+		delete[] rightHalf;
 	}
+} 
 
--- src/main1.cpp
+++ src/main1.cpp
@@ -8,12 +8,11 @@ int main() {
 	int n = 9;
 	msort<int>(mas, n);
 	for (int i = 0; i < n; i++) {
-		std::cout << mas[i] << " ";
+		cout << mas[i] << endl;
 	}
-        std::cout << endl;
 	char* val[6]{ (char*)"ffff", (char*)"fff2", (char*)"f", (char*)"ff", (char*)"vvv", (char*)"fffrr" };
 	msort(val, 6);
 	for (int i = 0; i < 6; i++) {
-		std::cout << val[i] << endl;
+		cout << val[i] << endl;
 	}
 } 
