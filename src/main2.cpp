42e7f11d1c95566d032797a158e8f95619a15216
ignatovskiy <42871833+ignatovskiy@users.noreply.github.com>
2019-12-17 14:17:09 +0300

    fix

--- src/main2.cpp
+++ src/main2.cpp
@@ -5,7 +5,7 @@ int main()
 {
     const size_t array_length = 5;
     int* dynamic_array;
-    dynamic_array = createArr<int, array_length>(&gen);
+    dynamic_array = createArr<int, array_length>(my_gen);
     for (size_t i = 0; i < array_length; i++)
         std::cout << dynamic_array[i] << " ";
     return 0;
--- src/main3.cpp
+++ src/main3.cpp
@@ -5,14 +5,14 @@ int main()
 {
     const size_t int_array_length = 5;
     int int_array[int_array_length]{10, 20, 30, 40, 50};
-    map<int, int_array_length>(int_array, change);
+    map<int, int_array_length>(int_array, my_change);
     for (size_t i = 0; i < int_array_length; i++)
         std::cout << int_array[i] << " ";
     std::cout << std::endl;
 
     const size_t double_array_length = 5;
     double double_array[double_array_length]{10.1, 5.5, 20.6, 31.5, 302.0};
-    map<double, double_array_length>(double_array, change);
+    map<double, double_array_length>(double_array, my_change);
     for (size_t i = 0; i < double_array_length; i++)
         std::cout << double_array[i] << " ";
     return 0;
